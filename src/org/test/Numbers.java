4e8e6d63de5bf6ec5dbdde04e9c39a9d58a1463e
veena1395 <veena131095@gmail.com>
2019-04-29 10:04:19 +0530

    hai

---
+++ src/org/test/Numbers.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class Numbers {
+	public static void main(String[] args) {
+		for (int i = 4; i > 0; i--) {
+
+			for (int j = 0; j < i; j++) {
+				System.out.println(j + 1);
+
+			}
+			System.out.println("");
+		}
+	}			
+		
+	}
